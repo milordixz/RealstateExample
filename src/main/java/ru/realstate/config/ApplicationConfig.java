f02c9bafd7f895dff5d3a0cfa24357ca02ad1af8
MiLoRD <milordixz@gmail.com>
2016-09-18 16:03:12 +0600

    RS:0001 - Создание проекта Spring MVC,Security,JPA, Thymeleaf

---
+++ src/main/java/ru/realstate/config/ApplicationConfig.java
@@ -0,0 +1,22 @@
+package ru.realstate.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+
+import ru.realstate.Application;
+
+@Configuration
+@PropertySource("classpath:persistence-pgsql.properties")
+@PropertySource("classpath:application.properties")
+@ComponentScan(basePackageClasses = Application.class)
+class ApplicationConfig {
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
+    }
+
+}
